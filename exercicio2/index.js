44b332141edecd33bbe1a0320c433e5010dd5047
daniellehonoria <daniellehonoria@gmail.com>
2022-08-29 17:49:00 -0300

    Resolução exercício 2

---
+++ exercicio2/index.js
@@ -0,0 +1,12 @@
+let pergunta1 = "Você pegou covid?"
+let resposta1 = prompt(pergunta1)
+
+let pergunta2 = "Você precisou ser hospitalizado?"
+let resposta2 = prompt(pergunta2)
+
+let pergunta3 = "Você ficou sequelado?"
+let resposta3 = prompt(pergunta3)
+
+console.log("Você pegou covid?", resposta1)
+console.log("Você precisou ser hospitalziado?" , resposta2)
+console.log("Você ficou sequelado?" , resposta3)
